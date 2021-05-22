19e13187e41cb3e765311b428fd8dc64d40e6139
Aaron <abautista@canteradigital.mx>
2021-05-22 18:12:37 -0500

    test con varios viewModels antes de agregar el before

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,16 +3,18 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.not
-import org.hamcrest.Matchers.nullValue
+import org.hamcrest.Matchers.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
@@ -30,6 +32,17 @@ class TasksViewModelTest {
         val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
 
         assertThat(value.getContentIfNotHandled(), not(nullValue()))
+
+
+    }
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible(){
+
+         val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+         taskViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        assertThat(taskViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
 
 
     }
