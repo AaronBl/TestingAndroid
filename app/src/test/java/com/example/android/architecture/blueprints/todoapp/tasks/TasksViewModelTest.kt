9a67feab1cf0c9b4c5d44b12650086a62efd66e1
Aaron <abautista@canteradigital.mx>
2021-05-22 18:42:40 -0500

    test con varios viewModels usanso Before

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,7 +3,6 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
@@ -18,12 +17,23 @@ class TasksViewModelTest {
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
+    // Subject under test
+    private lateinit var tasksViewModel: TasksViewModel
+
+    //Cuando tenga un código de configuración repetido para varias pruebas,
+    // puede usar la anotación @Before para crear un método de configuración y eliminar el código repetido.
+    // Dado que todas estas pruebas van a probar TasksViewModely necesitan un modelo de vista.
+    @Before
+    fun setupViewModel(){
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
+
 
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
         // Given a fresh ViewModel
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
         // When adding a new task
         tasksViewModel.addNewTask()
@@ -38,11 +48,9 @@ class TasksViewModelTest {
     @Test
     fun setFilterAllTasks_tasksAddViewVisible(){
 
-         val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
 
-         taskViewModel.setFiltering(TasksFilterType.ALL_TASKS)
-
-        assertThat(taskViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
+        assertThat(tasksViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
 
 
     }
